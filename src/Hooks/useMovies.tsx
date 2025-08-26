147ef84019a563b7f5ea517254b4ba9959ecf439
looju <omofade2020@gmail.com>
2025-08-26 20:34:23 +0100

    query functions

---
+++ src/Hooks/useMovies.tsx
@@ -0,0 +1,47 @@
+import {
+  fetchMovieCredits,
+  fetchMovieDetails,
+  fetchMovies,
+  fetchSearchMovies,
+} from "@/api/movies";
+import { useQuery } from "@tanstack/react-query";
+
+export const useMovies = (category: string) =>
+  useQuery({
+    queryKey: ["movies", category],
+    queryFn: () => fetchMovies(category),
+    enabled: true,
+    refetchInterval: 6000,
+    staleTime: 20000,
+  });
+
+export const useMovieDetails = (id: string) =>
+  useQuery({
+    queryKey: ["movieDetails", id],
+    queryFn: () => fetchMovieDetails(id),
+    enabled: !!id,
+    refetchInterval: 60000,
+  });
+
+export const useMoviesCasts = (id: string) =>
+  useQuery({
+    queryKey: ["movieCasts", id],
+    queryFn: () => fetchMovieCredits(id),
+    enabled: !!id,
+    refetchInterval: 60000,
+  });
+
+export const useSearch = (query: string) =>
+  useQuery({
+    queryKey: ["searchMovies", query],
+    queryFn: () => fetchSearchMovies(query),
+    enabled: !!query,
+    refetchInterval: 60000,
+  });
+
+  export const usePopularMovies = () =>
+  useQuery({
+    queryKey: ["popular-movies"],
+    queryFn: fetchPopularMovies,
+    refetchInterval: 60000,
+  });
